f97e84b1b34f14a529e3a69d711188c609875173
Lucasarret <95381695+Lucasarret@users.noreply.github.com>
2021-12-02 17:28:43 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,22 @@
+var idade=[14,13,19,7];
+var soma=0;
+var mediadeidade=0;
+for (i=0;i<idade.length;i++){
+console.log(idade[i])
+soma=soma+idade[i];
+}
+console.log(soma);
+mediadeidade=soma/idade.lenght;
+console.log(mediadeidade);
+console.log("opa");
+function setup() 
+{
+  createCanvas(400,400);
+}
+
+function draw() 
+{
+background(51);
+
+}
+
